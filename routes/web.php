abdf111cd9c1b377a1668186c8071b3348832cc4
Mickey <r0740474@student.thomasmore.be>
2019-12-22 13:39:40 +0100

    Opdracht 9: <= Admin: records

--- routes/web.php
+++ routes/web.php
@@ -41,7 +41,7 @@ Route::middleware(['auth', 'admin'])->prefix('admin')->group(function () {
     Route::redirect('/', 'records');
     Route::get('genres/qryGenres', 'Admin\GenreController@qryGenres');
     Route::resource('genres', 'Admin\GenreController');
-    Route::get('records', 'Admin\RecordController@index');
+    Route::resource('records', 'Admin\RecordController');
 
 });
 
